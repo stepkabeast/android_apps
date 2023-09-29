52da823e5093d6eb083783122e0ca76bcb0b6f5b
Kovalev Stepan <stepak038@gmail.com>
2023-09-30 00:19:50 +0800

    Add files via upload

---
+++ Comparable/Main.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<TransferMarket.FootballPlayers> players = new ArrayList<>();
+        TransferMarket tm = new TransferMarket();
+
+
+        players.add(tm.new Forwards(10, "Benzema",
+                "Real Madrid", 5));
+        players.add(tm.new Defenders(5, "Varan",
+                "PSG", 8));
+        players.add(tm.new Forwards(13, "Aguero",
+                "Manchester city", 7));
+        players.add(tm.new Defenders(8, "Alaba",
+                "Bayaren", 10));
+
+
+
+        //System.out.println("Игроки до сортировки:");
+        for (TransferMarket.FootballPlayers player : players) {
+            System.out.println(player);
+
+
+        }
+        for (TransferMarket.FootballPlayers player : players){
+            System.out.println("КПД игрока " + player.getName()
+                    + ": " + player.getGameStat());
+        }
+
+        Collections.sort(players);
+
+        System.out.println("-----------------------------");
+        for (TransferMarket.FootballPlayers player: players) {
+            System.out.println("КПД игрока " + player.getName()
+                    + ": " + player.getGameStat());
+        }
+    }
+}
---
+++ Comparable/TransferMarket.java
@@ -0,0 +1,142 @@
+public class TransferMarket {
+
+    abstract class FootballPlayers implements Comparable<FootballPlayers> {
+        protected String name;
+        protected String club;
+        protected int games_count;
+
+        public FootballPlayers(String name, String club, int games_count) {
+            this.name = name;
+            this.club = club;
+            this.games_count = games_count;
+
+        }
+
+
+        public abstract int getGameStat();
+        public String getName(){
+            return name;
+        }
+        public String getClub(){
+            return club;
+        }
+        public int getGamesNumber(){
+            return games_count;
+        }
+
+        @Override
+        public String toString(){
+            return "Имя игрока: " + name + "    " + "Клуб: " + club;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            FootballPlayers player = (FootballPlayers) obj;
+            return player.equals(player.name);
+
+        }
+
+        @Override
+        public int compareTo(FootballPlayers player) {
+            if (this.getGameStat() < player.getGameStat()) {
+                return -1;
+            }
+
+            if (this.getGameStat() > player.getGameStat()) {
+                return 1;
+            }
+
+            return 0;
+        }
+
+
+    }
+
+     class Forwards extends FootballPlayers {
+        private int goals;
+
+
+         public Forwards(int goals, String name, String club,int games_count) {
+             super(name, club, games_count);
+             this.goals = goals;
+
+         }
+
+
+         @Override
+         public int getGameStat(){
+            return (goals * games_count);
+        }
+
+        @Override
+         public String toString(){
+            return "Имя: " + name + " \n" + "Клуб: " + club + " \n" + "Голов: " + goals + " \n";
+        }
+
+         @Override
+         public boolean equals(Object obj) {
+             if (this == obj) {
+                 return true;
+             }
+
+             if (obj == null || getClass() != obj.getClass()) {
+                 return false;
+             }
+
+             if (!super.equals(obj)) {
+                 return false;
+             }
+
+             Forwards forward = (Forwards) obj;
+             return goals == forward.goals;
+         }
+
+    }
+
+    class Defenders extends FootballPlayers {
+        private int interceptions;
+
+        public Defenders(int interceptions,String name, String club, int games_count) {
+            super(name, club, games_count);
+            this.interceptions = interceptions;
+        }
+
+
+        @Override
+        public int getGameStat(){
+            return (interceptions*games_count);
+        }
+
+        @Override
+        public String toString(){
+            return "Имя: " + name + " \n" + "Клуб: " + club + " \n" + "Перехватов: " + interceptions + " \n";
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) {
+                return true;
+            }
+
+            if (obj == null || getClass() != obj.getClass()) {
+                return false;
+            }
+
+            if (!super.equals(obj)) {
+                return false;
+            }
+
+            Defenders defender = (Defenders) obj;
+            return interceptions == defender.interceptions;
+        }
+
+    }
+}
+
